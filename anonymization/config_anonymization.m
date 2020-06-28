733133b4016ca01b10774be4c2d29164828b23e2
Dorien van Blooijs <d.vanblooijs@umcutrecht.nl>
2020-06-28 09:53:54 +0200

    added PRIOS study

--- anonymization/config_anonymization.m
+++ anonymization/config_anonymization.m
@@ -10,12 +10,12 @@ clear
 cfg.proj_dirinput = '/home/dorien/Desktop/temp_ecog/';
 
 %% type patient decoding-number
-tempName = input('RESPectname/REC2Stimname (e.g. [RESP0733] / [REC2Stim01]): ','s');
+tempName = input('RESPectname/REC2Stimname (e.g. [RESP0733] / [REC2Stim01] / [PRIOS01]): ','s');
 
 % check whether RESP-number is entered correctly
 if strcmp(tempName,'') && ~isempty(respName)
     
-elseif contains(tempName,'RESP') || contains(tempName,'REC2Stim')
+elseif contains(tempName,'RESP') || contains(tempName,'REC2Stim') || contains(tempName,'PRIOS')
     respName = tempName;
 else
     error('RESPect/REC2Stim name is not correct')
--- trc2bids/config_trc2bids.m
+++ trc2bids/config_trc2bids.m
@@ -19,7 +19,7 @@ clear
 
 %% folder settings and selecting the correct patient
 
-cfg(1).sub_labels = {['sub-' input('Patient number (RESPXXXX)/(REC2StimXX): ','s')]};
+cfg(1).sub_labels = {['sub-' input('Patient number (RESPXXXX)/(REC2StimXX)/(PRIOSXX): ','s')]};
 
 % CLE
 % cfg(1).proj_dirinput = '/home/dorien/Desktop/bulk/smb-share:server=smb-ds.bsc01.gd.umcutrecht.nl,share=ds_her_respect-leijten/Dorien/c_ecog/sz_cle/RESPect_sz_scratch/patients';
@@ -33,19 +33,25 @@ if contains(cfg(1).sub_labels,'RESP')
     elseif strcmp(foldername,'RESPect_spes_scratch')
         cfg(1).proj_dirinput = '/home/dorien/Desktop/db_respect/Dorien/c_ecog/spes/RESPect_spes_scratch/patients';
     elseif strcmp(foldername,'RESPect_chronic_ECoG_trc')
-        cfg(1).proj_dirinput = '/home/dorien/Desktop/bulk_respect/smb-share:server=arch11-smb-ds.arch11.gd.umcutrecht.nl,share=her$/snap/Respect-Leijten/RESPect_chronic_ECoG_trc/patients';
+        cfg(1).proj_dirinput = '/home/dorien/Desktop/bulkstorage/remote_conn/smb-share:server=arch11-smb-ds.arch11.gd.umcutrecht.nl,share=her$/snap/Respect-Leijten/RESPect_chronic_ECoG_trc/patients';
     else
        error('Foldername is not recognized') 
     end
-    cfg(2).proj_dirinput = '/Fridge/chronic_ECoG';
-    cfg(1).proj_diroutput = '/Fridge/chronic_ECoG';
-    cfg(2).proj_diroutput = '/Fridge/CCEP'; % optional: this could remain empty
+    cfg(2).proj_dirinput = '/Fridge/KNF/chronic_ECoG';
+    cfg(1).proj_diroutput = '/Fridge/KNF/chronic_ECoG';
+    cfg(2).proj_diroutput = '/Fridge/KNF/CCEP'; % optional: this could remain empty
     
 elseif contains(cfg(1).sub_labels,'REC2Stim')
     % REC2Stim
     cfg(1).proj_dirinput = '/home/dorien/Desktop/db/Dorien/REC2Stim/patients';
-    cfg(2).proj_dirinput = '/Fridge/REC2Stimstudy';
-    cfg(1).proj_diroutput = '/Fridge/REC2Stimstudy';
+    cfg(2).proj_dirinput = '/Fridge/KNF/REC2Stimstudy';
+    cfg(1).proj_diroutput = '/Fridge/KNF/REC2Stimstudy';
+elseif contains(cfg(1).sub_labels,'PRIOS')
+    % prios study
+    cfg(1).proj_dirinput = '/home/dorien/Desktop/bulkstorage/db/respect-leijten/PRIOS_study/patients';
+    cfg(2).proj_dirinput = '/Fridge/KNF/REC2Stimstudy/PRIOS_study';
+    cfg(1).proj_diroutput = '/Fridge/KNF/REC2Stimstudy/PRIOS_study';
+    
 end
 
 % [~, pathname] = uigetfile('*.TRC;*.trc','Select *.TRC file',[cfg(1).proj_dirinput]);
